e8bb579cedc8eab581a8dba876a739dce038e827
thejey99 <john99ryan@gmail.com>
2026-07-03 20:22:13 -0400

    Add GAME_LESSONS to SEED_LESSONS array

--- src/lessons/seed.ts
+++ src/lessons/seed.ts
@@ -2,9 +2,11 @@ import type { Lesson } from '../types';
 import { JS_LESSONS } from './js';
 import { TS_LESSONS } from './ts';
 import { PY_LESSONS } from './py';
+import { GAME_LESSONS } from './games';
 
 export const SEED_LESSONS: Lesson[] = [
   ...JS_LESSONS,
   ...TS_LESSONS,
   ...PY_LESSONS,
+  ...GAME_LESSONS,
 ];
